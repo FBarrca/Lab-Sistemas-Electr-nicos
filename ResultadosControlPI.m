0749ded580c460577c4b2a9b2d95e31eacd94872
Barrca <60709314+FranciscoBarCa@users.noreply.github.com>
2022-10-04 12:05:09 +0200

    Add files via upload

---
+++ ResultadosControlPI.m
@@ -0,0 +1,7 @@
+ 
+load pifinal
+ensayo=EnsayoPIFINAL;
+t =[0:0.01:2.98];
+plot(t', ensayo(:,3))
+figure;
+plot(t', ensayo(:,2))
--- resultados.m
+++ resultados.m
@@ -2,7 +2,7 @@
 kpplanta  = 10.467 
 Tp1 = 0.02
 
-kpplanta/(1+Tp1*tf('s'))
+Plant = kpplanta/(1+Tp1*tf('s'))
 
 Kp	=0.31596
 Ki	=50.4885
