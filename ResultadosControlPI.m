54f5960accbebba2af17e0b6f9fcf0b43bf1ab2f
FranciscoBarCa <60709314+FranciscoBarCa@users.noreply.github.com>
2022-10-21 00:02:56 +0200

    Fin de informe p2

--- ResultadosControlPI.m
+++ ResultadosControlPI.m
@@ -1,7 +1,25 @@
- 
 load pifinal
-ensayo=EnsayoPIFINAL;
-t =[0:0.01:2.98];
-plot(t', ensayo(:,3))
-figure;
-plot(t', ensayo(:,2))
+ensayo = EnsayoPIFINAL;
+t = [0:0.01:2.98];
+
+% Create a figure with the output on top and the drive function at the bottom
+hFig = figure
+
+%  Subplot for output
+hSub(1) = subplot(2, 1, 1);
+
+hold on
+plot(t', ensayo(:, 1))
+plot(t', ensayo(:, 3))
+title('Entrada y Salida del Sistema de Control PI')
+xlabel('Tiempo (s)')
+ylabel('Luminosidad (lx)')
+legend('Referencia', 'Salida')
+
+%  Subplot for drive function
+hSub(2) = subplot(2, 1, 2);
+plot(t', ensayo(:, 2))
+title('Función de mando')
+xlabel('Tiempo (s)')
+ylabel('PWM (0/000)')
+legend('Mando')
