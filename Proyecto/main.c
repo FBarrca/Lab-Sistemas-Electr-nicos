e7da9b43ff3b9fe707d87d19bef917463922e712
FranciscoBarCa <60709314+FranciscoBarCa@users.noreply.github.com>
2022-10-26 00:11:05 +0200

    Propuesta screen rotate

---
+++ Proyecto/main.c
@@ -0,0 +1,10 @@
+//  Parallel scara robot with 2 DOF with a depth sensor
+// Developed in FREE RTOS
+//  Created on: 2022-10-22
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+int main(void)
+{
